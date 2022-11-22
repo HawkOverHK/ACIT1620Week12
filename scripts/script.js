85e7912dd791c51a1e166675d8691971e9268f62
Norvin <chnorvin@gmail.com>
2022-11-22 14:54:45 -0800

    Once only

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,8 @@ const clickButton = document.querySelector('#click');
 function clickHandler()
 {
     alert("BONK")
+    // clickButton.removeEventListener("click",clickHandler)
 }
-clickButton.addEventListener("click", clickHandler)
+clickButton.addEventListener("click", clickHandler, {once:true})
+// this is alt method the {once:true}
+
